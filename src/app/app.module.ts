96d31892fa394fd104ca13513d43df1858487bb4
unknown <adshayani.18@itfac.mrt.ac.lk>
2021-04-15 11:15:22 +0530

    frst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { EmailComposer } from '@ionic-native/email-composer/ngx'; 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [EmailComposer,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   subject="";
+   body="";
+   to="";
+  constructor(public navCtrl: NavController, public emailComposer: EmailComposer) {}
+  
+  send(){
+         let email = {
+           to: this.to,
+           attachment: [],
+           subject:this.subject,
+           body:this.body,
+           isHtml:false,
+           //this.email.addAlias('gmail', 'com.google.android.gm');
+           app:'Gmail'
 
-  constructor() {}
-
+           
+         }
+         this.emailComposer.open(email);
+  }
 }
